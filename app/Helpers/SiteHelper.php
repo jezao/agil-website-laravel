d1932490e40215e4933bfdb4cbbb686b6a9aa365
Jefferson Silva <jefferson.silva@foleon.com>
2024-10-29 23:46:47 +0100

    update gtag

--- app/Helpers/SiteHelper.php
+++ app/Helpers/SiteHelper.php
@@ -10,24 +10,28 @@ class SiteHelper
             'phone' => '15 99829-0671',
             'email' => 'contato@desentope.je',
             'ga' => 'UA-123456789-1',
+            'gtag' => 'GTM-P6BDCDV9',
         ],
         'desentupidorsorocaba.com.br' => [
             'title' => 'Desentupidor Sorocaba',
             'phone' => '15 99829-0671',
             'email' => 'contato@desentupidorsorocaba.com.br',
             'ga' => 'G-Q2SMPZKGSS',
+            'gtag' => 'GTM-P6BDCDV9',
+
         ],
         'joaodesentupidor.com.br' => [
             'title' => 'João Desentupidor em Sorocaba',
             'phone' => '15 99829-0671',
             'email' => 'contato@joaodesentupidor.com.br',
             'ga' => 'G-Q2SMPZKGSS',
+            'gtag' => 'GTM-P6BDCDV9',
         ],
     ];
 
     static function config($key): string
     {
-        return self::SITE_CONFIG[request()->getHost()][$key];
+        return self::SITE_CONFIG[request()->getHost()][$key] ?? '';
     }
 
 }
